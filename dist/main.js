464b47b5e076ea5770760880a4785240e387ec14
Hermine Bschlangaul <hermine.bschlangaul@gmx.net>
2022-09-07 09:02:16 +0200

    Warnhinweis ausblenden

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env -S node --experimental-specifier-resolution=node
+#! /usr/bin/env -S node --experimental-specifier-resolution=node --no-warnings
 // https://unix.stackexchange.com/a/657774
 import programm from './kommando-zeile';
 programm.parse(process.argv);
